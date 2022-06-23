14607309a325feee0d74de0429866e748b79c807
ScottMastro <scott-mastro@hotmail.com>
2022-06-23 17:02:58 -0400

    debug line added

--- lib/bot.rb
+++ lib/bot.rb
@@ -124,7 +124,7 @@ class DiscordDatastore::Bot
         bot.message do |event|
           if ! event.author.bot_account
 
-            #event.respond event.content
+            DiscordDatastore::BotInstance::send event.content
 
             if DiscordDatastore::BotInstance::add_message
               DiscordDatastore::BotInstance.sync true
